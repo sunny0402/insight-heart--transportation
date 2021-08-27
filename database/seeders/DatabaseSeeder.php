5eb1e94fa8e0f84c89bf8d65f7aa0d792f49aa09
sunny0402 <47863780+sunny0402@users.noreply.github.com>
2021-08-27 16:23:53 -0400

    update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,40 @@ class DatabaseSeeder extends Seeder
         Role::create(['name' => 'driver']);
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'client']);
+
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin123@mail.ca',
+            'email_verified_at' => now(),
+            'role_id' => '2',
+            'address' => '2 Yonge Street',
+            'phone_number' => '4161236677',
+            'password' => bcrypt('12344321'),
+            'created_at' => now(),
+        ]);
+
+        User::create([
+            'name' => 'Test Driver 1',
+            'email' => 'driver123@mail.ca',
+            'email_verified_at' => now(),
+            'role_id' => '1',
+            'address' => '1 Yonge Street',
+            'phone_number' => '4161231111',
+            'password' => bcrypt('1234'),
+            'created_at' => now(),
+        ]);
+
+        User::create([
+            'name' => 'Test Client',
+            'email' => 'client123@mail.ca',
+            'email_verified_at' => now(),
+            'role_id' => '3',
+            'address' => '3 Yonge Street',
+            'phone_number' => '4161233333',
+            'description' => 'A description of 25 characters.',
+            'password' => bcrypt('1234'),
+            'created_at' => now(),
+
+        ]);
     }
 }
